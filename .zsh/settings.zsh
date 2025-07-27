a8a42864121355b4c32ab02c063ca46ef5734a57
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-07-26 22:23:17 -0700

    Update zsh settings

--- .zsh/settings.zsh
+++ .zsh/settings.zsh
@@ -1,3 +1,10 @@
 # Case insensitive completion
 autoload -Uz +X compinit && compinit
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+
+# Vim keybindings
+bindkey -v
+
+# Wait only 0.01 seconds for the rest of a key sequence
+export KEYTIMEOUT=1
+
