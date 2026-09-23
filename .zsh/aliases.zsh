53265a7eb9e23fbb5c9815edf58b61f7ca71a8f6
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2026-09-22 18:47:57 -0700

    Update aliases and rename agt.md to agts.md

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -11,10 +11,10 @@ alias opc="opencode"
 
 # Copy AGENTS.md to various locations
 function cpai() {
-    cp -v ~/Developer/AGENTS.md ~/.config/zed/AGENTS.md
-    cp -v ~/Developer/AGENTS.md ~/.pi/agent/APPEND_SYSTEM.md
-    cp -v ~/Developer/AGENTS.md ~/.config/opencode/AGENTS.md
-    cp -v ~/Developer/AGENTS.md ~/.fx/AGENTS.md
+    cp -v ~/AGTS.md ~/.config/zed/AGENTS.md
+    cp -v ~/AGTS.md ~/.pi/agent/APPEND_SYSTEM.md
+    cp -v ~/AGTS.md ~/.config/opencode/AGENTS.md
+    cp -v ~/AGTS.md ~/.fx/AGENTS.md
 }
 
 # Alias to open Visual Studio Code Insiders
